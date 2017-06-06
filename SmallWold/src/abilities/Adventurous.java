0ab0aa467d9386af8a7111fe4e171046ccd39898
Marinus <Marinus@145.101.73.142>
2017-06-06 11:59:05 +0200

    heel veel dingen

--- SmallWold/src/abilities/Adventurous.java
+++ SmallWold/src/abilities/Adventurous.java
@@ -1,11 +1,28 @@
 package abilities;
 
+import java.util.List;
+
+import listCreators.AbilityListCreator;
+import playBoard.Map;
+import player.Player;
+
 public class Adventurous extends Ability
 {
+	private int abilityIncome;
+	private int coins;
+	private List<Player> playerList;
+	AbilityListCreator abilityList = new AbilityListCreator();
+	Map map;
+	Player activePlayer;
 	public Adventurous()
 	{
 		amountOfTokens = 5;
 		type = "Adventurous";
 		traitText = "+1 coin for each popular place";
+	}
+
+	public void calculateAbilityIncome(Player activePlayer){
+		this.activePlayer = activePlayer;
+
 	}
 }
--- SmallWold/src/controllers/EndTurnController.java
+++ SmallWold/src/controllers/EndTurnController.java
@@ -1,20 +1,28 @@
 package controllers;
 
+import java.util.List;
+
 import listCreators.AbilityListCreator;
 import main.Ammy;
 import playBoard.Map;
+import player.Hand;
 import player.Player;
 import setup.MapCreator;
 
 public class EndTurnController
 {
-	private int incomeOfTerrains;
 	private int terrainIncome;
-	private int abilityIncome;
-	private int raceIncome;
 	private int relicIncome;
 	private int specialPlaceIncome;
 	private int totalSum;
+	private int coins;
+	private int terrainCounter;
+	private int terrainStringCounter;
+	private int elementCounter;
+	private int terrain;
+	private int value;
+	private List<Player> playerList;
+
 	AbilityListCreator abilityList = new AbilityListCreator();
 	Map map;
 	Player activePlayer;
@@ -22,42 +30,48 @@ public class EndTurnController
 	public EndTurnController(Ammy ammy)
 	{
 		this.map = ammy.getMap();
+		this.playerList = ammy.getPlayerList();
 	}
 
 	public void calculateTerrainIncome(Ammy ammy)
 	{
 		this.activePlayer = ammy.getPlayerList().get(0);
-		incomeOfTerrains = 0;
+		terrainIncome = 0;
 
 		for(int terrainCounter=0;terrainCounter<map.getAllTerrains().size();terrainCounter++)		//As long as there are terrains
 		{
 			if (activePlayer.getActiveSet().getRace().equals(map.getTerrain(terrainCounter).getRace()))
 			{
-				incomeOfTerrains++;
+				terrainIncome++;
 			}
 		}
 	}
 
-	public int getIncomeOfTerrains()
+
+	public void checkTerrainType(String terrainString)
 	{
-		return incomeOfTerrains;
-	}
-
-
-	public void calculateAbilityIncome(Player activePlayer){
-		this.activePlayer = activePlayer;
-
-		if (activePlayer.getActiveSet().getAbility().equals(abilityList.getListElement(1))){ // Adventurous
-
+		terrainCounter = 0;
+		elementCounter = 0;
+		terrain = 0;
+		value = 0;
+		terrainStringCounter = 0;
+		while(terrainCounter<map.getAllTerrains().size())				//While there's still terrains left
+		{
+			while(elementCounter<map.getTerrain(terrain).getIdArray().length)		//While there's still numbers in the terrain's array
+			{
+				if(map.getTerrain(terrain).getTerrainName().equals(terrainString)) 		//If the idCode is found, set isAttackable to true
+				{
+					terrainStringCounter++;
+					System.out.println("A: " + (terrain+1) + " is a " + terrainString);
+				}
+				value++;											//Look at the next value in the terrain's array, "eye"
+				elementCounter++;									//Keep track of which number in the array we're at
+			}
+			value = 0;												//"Eye" back at number 0 in the array
+			elementCounter = 0;										//Back at number 0 in a fresh terrain
+			terrainCounter++;										//Keep track of which terrain we're at
+			terrain++;												//Look at the next terrain, "eye"
 		}
-
-		if (activePlayer.getActiveSet().getAbility().equals(abilityList.getListElement(2))){ // Fisher
-
-
-		}
-	}
-	public void calculateRaceIncome(Player player){
-
 	}
 
 	public void calculateRelicIncome(Player player){
@@ -69,14 +83,13 @@ public class EndTurnController
 	}
 
 	public void addSum(){
-
+		totalSum = this.getTerrainIncome() + this.getAbilityIncome() + this.getRaceIncome() + this.getRelicIncome() + this.getSpecialPlaceIncome();
 	}
 
-	public void calculateNewBalance(){
-
+	public void calculateNewBalance(int coins){
+	coins = playerList.get(0).getCoins() + this.getTotalSum();
+	playerList.get(0).setCoins(coins);
 	}
-
-
 
 	public int getTerrainIncome() {
 		return terrainIncome;
@@ -84,22 +97,6 @@ public class EndTurnController
 
 	public void setTerrainIncome(int terrainIncome) {
 		this.terrainIncome = terrainIncome;
-	}
-
-	public int getAbilityIncome() {
-		return abilityIncome;
-	}
-
-	public void setAbilityIncome(int abilityIncome) {
-		this.abilityIncome = abilityIncome;
-	}
-
-	public int getRaceIncome() {
-		return raceIncome;
-	}
-
-	public void setRaceIncome(int raceIncome) {
-		this.raceIncome = raceIncome;
 	}
 
 	public int getRelicIncome() {
@@ -125,6 +122,7 @@ public class EndTurnController
 	public void setTotalSum(int totalSum) {
 		this.totalSum = totalSum;
 	}
+
 
 
 
