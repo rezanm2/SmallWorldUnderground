830d6905644172d27fab2c3595ee12c0c7e4b8a0
Reza Naser <rezanm2@gmail.com>
2017-06-07 14:21:32 +0200

    Player can choose a new set and then set it on decline.

--- SmallWold/src/main/Decline.java
+++ SmallWold/src/main/Decline.java
@@ -1,4 +1,6 @@
 package main;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -50,6 +52,7 @@ public  class Decline {
 		Player activePlayer;
 		AbilityListCreator abilityList;
 		RaceListCreator raceList;
+		List<Player> playerList;
 		Ability [] arrAbility = {new Adventurous(), new Fisher(), new Flocking(), new Frightened(), new Immortal(), new Magic(),
 				new Martyr(), new Mining(), new Muddy(), new Mystic(), new Quarreling(), new Reborn(), new Royal(), new Shield(), 
 				new Stone(), new Thieving(), new Tomb(), new Vampire(), new Vanishing(), new Vengeful(), new Wise()};
@@ -57,18 +60,20 @@ public  class Decline {
 		Race [] arrRaces = {new Cultists(), new Drow(), new Flames(), new Gnomes(), new Kraken(), new Liches(), new Lizardmen(),
 				new Monster(), new Mudmen(), new Mummies(), new Ogres(), new ShadowMimes(), new Shrooms(), new Spiderines(), new WillOWisp()};
 		
+		
 		public Decline(Ammy ammy){
 			
 			this.activePlayer = ammy.getActivePlayer();
 			this.abilityList = ammy.getAbilityList();
 			this.raceList = ammy.getRaceList();
+			this.playerList = ammy.getPlayerList();
 		}
 		public void goInDecline(){
 	
 			System.out.println("Do you want to set your race and ability to decline?");
 			Scanner scanner = new Scanner(System.in);
 			String yesOrNo = scanner.nextLine();
-//			while( yesOrNo != "yes" || yesOrNo != "no")
+//			while( !yesOrNo.equals("yes") || !yesOrNo.equals("no"))
 //			{
 //				System.out.print("Say yes or no");
 //				yesOrNo = scanner.nextLine();
@@ -79,7 +84,7 @@ public  class Decline {
 				//btnDecline.setOnAction(e -> 
 				//{
 				 activePlayer.setDeclineSet(activePlayer.getActiveSet());
-				 System.out.println("Declined");
+				 System.out.println(activePlayer.getDeclineSet().getRace().getTokenType() + " and " +activePlayer.getDeclineSet().getAbility().getType() + " for player: " + activePlayer.getName().toString() +  " are declined");
 				 //	});
 			}
 			if(yesOrNo.equals("no"))
@@ -114,21 +119,33 @@ public  class Decline {
 	}
 		public void chooseNewSet(){
 			Set tempSet;
+//			System.out.println(arrNewAbility.length);
+//			for ( int i = 0 ; i < arrRaces.length - playerList.size() ; i++ )
+//			   {
+//					arrRaces[ i ] = arrRaces[ i + 1 ];
+//			   }
+//			
+//			for(int x=0;x<arrNewAbility.length;x++){
+//				System.out.print((x+1) + ": " +  arrNewAbility[x].getType());
+//				System.out.println();
+//			}
 			for(int x=0;x<6;x++){
-				System.out.print((x+1) + ": " +  arrAbility[x].getType() + " " + arrRaces[x].getTokenType());
-				
+				System.out.print((x+1) + ": " +  arrAbility[x].getType() + " " +  arrRaces[x].getTokenType());
 				System.out.println();
 			}
+			
 			Scanner input = new Scanner(System.in);
 			System.out.println("Choose a new set: ");
 			int setNr = input.nextInt();
+			
 			while(setNr < 1 || setNr > 6){
 				System.out.println("Enter the set number: ");
 				setNr = input.nextInt();
 			}
 			tempSet = new Set(arrAbility[setNr-1], arrRaces[setNr-1]);
 			activePlayer.setActiveSet(tempSet);
-			System.out.println(activePlayer.getActiveSet().getAbility());
-			System.out.println(activePlayer.getActiveSet().getRace());
+			System.out.println(playerList.get(0).getActiveSet().getAbility().getType() + " and " + playerList.get(0).getActiveSet().getRace().getTokenType() + " are activated.");
+			
+
 		}
 }
