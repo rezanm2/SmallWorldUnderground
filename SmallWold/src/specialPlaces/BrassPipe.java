dc0db2488967687a735594a2e37aa260abc87398
Bas Dorresteijn <basd1998@gmail.com>
2017-06-15 10:44:35 +0200

    BrassPipe works

--- SmallWold/src/specialPlaces/BrassPipe.java
+++ SmallWold/src/specialPlaces/BrassPipe.java
@@ -1,9 +1,18 @@
 package specialPlaces;
 
+import controllers.MapTester;
+import controllers.TerrainController;
 import main.Ammy;
+import playBoard.Map;
 
 public class BrassPipe extends SpecialPlace
 {
+	private Map map;
+	private TerrainController terrainController;
+	private int terrainNumber;
+	private MapTester mapTester;
+	private boolean active = true;
+
 	public BrassPipe()
 	{
 		name = "Great Brass Pipe";
@@ -12,7 +21,30 @@ public class BrassPipe extends SpecialPlace
 
 	@Override
 	public void processSpecialPlace(Ammy ammy) {
-		// TODO Auto-generated method stub
-
+		this.map = ammy.getMap();
+		this.activePlayer = ammy.getActivePlayer();
+		this.terrainController = ammy.getTc();
+		this.mapTester = ammy.getTest();
+		for(int i = 0; i < map.getAllTerrains().size(); i++) {
+			if(map.getTerrain(i).getSpecialPlace().getName() == name)
+			{
+				terrainNumber = i;
+				break;
+			}
+		}
+		if(active == true && activePlayer.getActiveSet().getRace().getName() == map.getTerrain(terrainNumber).getRace().getName()) {
+			
+			System.out.println(map.getTerrain(terrainNumber).getTerrainName());
+			terrainController.checkAdjacentToTerrainType(map.getTerrain(terrainNumber).getTerrainName());
+			for(int x=0;x<map.getAllTerrains().size();x++)
+			{
+				System.out.println(!map.getTerrain(terrainNumber).getRace().getName().equals(activePlayer.getActiveSet().getRace().getName()));
+				if(map.getTerrain(x).getIsAdjacent() == true && !map.getTerrain(x).getRace().getName().equals(activePlayer.getActiveSet().getRace().getName()))
+				{
+					map.getTerrain(x).setIsAttackable(true);//This changes all area adjacent to chasm to attackable
+				}
+			}
+		}
+		
 	}
 }
