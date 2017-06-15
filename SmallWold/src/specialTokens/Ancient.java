5e2c5e03c05482fa4eafe77f6d49b78011a30e21
Reza Naser <rezanm2@gmail.com>
2017-06-15 13:44:40 +0200

    Ancient

--- SmallWold/src/specialTokens/Ancient.java
+++ SmallWold/src/specialTokens/Ancient.java
@@ -1,5 +1,4 @@
 package specialTokens;
-
 import java.util.Scanner;
 
 import controllers.CombatController;
@@ -17,26 +16,49 @@ public class Ancient extends SpecialToken {
 	private Player activePlayer;
 	private TerrainController terrainController;
 	Scanner scanner = new Scanner(System.in);
+	String yesOrNo = "";
 
 	@Override
 	public void processSpecialToken(Ammy ammy) {
 		this.map = ammy.getMap();
 		this.activePlayer = ammy.getActivePlayer();
+		this.terrainController = ammy.getTc();
+		this.mapTester = ammy.getTest();
+		this.cc = ammy.getCc();
+		
+			System.out.println("Do you want replace your Ancient? ");
+			yesOrNo = scanner.nextLine();
+			if(yesOrNo.equals("y")  || yesOrNo.equals("Y") )
+			{
+				if(map.getTerrain(terrainNumber).getRace().getName().equals("Cultists"))
+				{
+					System.out.println("On which terrain? ");
+					this.terrainNumber = scanner.nextInt()-1;
+					
+					terrainController.checkAdjacentToSingleTerrain(map.getTerrain(terrainNumber));
+					
+					if(map.getTerrain(terrainNumber).getIsAdjacent() && 
+							map.getTerrain(terrainNumber).getSpecialToken().equals(this))
+					{
+						changeTerrain(terrainNumber);
+						cc.setMiscModifier(1);
+					}
+					for(int x=0;x<map.getAllTerrains().size();x++)
+					{
+						System.out.println("Terrain number: " + (x+1) + " is changed to " + map.getTerrain(x).getSpecialToken());
 
-		terrainController.setAllRedeployableAreas(activePlayer);
-		mapTester.whichAreRedeployable(activePlayer);
-		System.out.println("Op welk terrein wil je de Ancient zetten?");
-		terrainNumber = scanner.nextInt();
-		changeTerrain(terrainNumber);
+					}
+					String yesOrNo = "";
+				}
 
-		if(map.getTerrain(terrainNumber).getRace() == activePlayer.getActiveSet().getRace()){
-			map.getTerrain(this.terrainNumber).setIsImmune(true);
+			}
+			if(yesOrNo.equals("N")  || yesOrNo.equals("n"))
+			{
+				System.out.println("Continue");
+				String yesOrNo = "";
+			}
 
-//			if(cc.getTerrain() == terrainController.checkAdjacentToSingleTerrain(map.getTerrain(this.terrainNumber)));
-//			cc.setMiscModifier(1);
-		}
 	}
-
 	public void changeTerrain(int terrainNumber) {
 		map.getTerrain(this.terrainNumber).setIsImmune(false);
 		map.getTerrain(this.terrainNumber).setSpecialToken(new Empty());
