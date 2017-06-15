d8703eaeb280e3d9a13e2f00f3f6bfe6dd93d0d3
Bas Dorresteijn <basd1998@gmail.com>
2017-06-15 11:53:39 +0200

    MineOfTheLostDwarf

--- SmallWold/src/specialPlaces/MineOfTheLostDwarf.java
+++ SmallWold/src/specialPlaces/MineOfTheLostDwarf.java
@@ -1,9 +1,14 @@
 package specialPlaces;
 
 import main.Ammy;
+import playBoard.Map;
 
 public class MineOfTheLostDwarf extends SpecialPlace
 {
+	private int terrainNumber;
+	private Map map;
+	private int specialPlaceIncome;
+
 	public MineOfTheLostDwarf()
 	{
 		name = "Mine of the Lost Dwarf";
@@ -12,7 +17,22 @@ public class MineOfTheLostDwarf extends SpecialPlace
 
 	@Override
 	public void processSpecialPlace(Ammy ammy) {
-		// TODO Auto-generated method stub
+		this.map = ammy.getMap();
+		for(int i = 0; i < map.getAllTerrains().size(); i++) {
+			if(map.getTerrain(i).getSpecialPlace().getName() == name)
+			{
+				terrainNumber = i;
+				break;
+			}
+		}
+		setSpecialPlaceIncome(2);
+	}
 
+	public int getSpecialPlaceIncome() {
+		return specialPlaceIncome;
+	}
+
+	public void setSpecialPlaceIncome(int specialPlaceIncome) {
+		this.specialPlaceIncome = specialPlaceIncome;
 	}
 }
