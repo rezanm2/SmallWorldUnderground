81177c2ecf8b589f54d025b2f65dd6e6d865794a
Bas Dorresteijn <basd1998@gmail.com>
2017-06-16 16:34:06 +0200

    Controller shiss stomme bin

---
+++ SmallWold/src/views/bottomBarView/BottomBarController.java
@@ -0,0 +1,14 @@
+package views.bottomBarView;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class BottomBarController {
+	
+	@FXML
+	private ImageView activeRace;
+	
+	@FXML
+	private ImageView activeAbility;
+
+}
