59ddf0814024e62a4cb27a53b0aecf469d7acbd9
Reza Naser <rezanm2@gmail.com>
2017-06-19 09:47:15 +0200

    -

--- SmallWold/src/views/bottomBarView/BottomBarController.java
+++ SmallWold/src/views/bottomBarView/BottomBarController.java
@@ -1,14 +1,28 @@
 package views.bottomBarView;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import player.Player;
 
 public class BottomBarController {
+	private Player selfPlayer;
 	
 	@FXML
 	private ImageView activeRace;
 	
 	@FXML
 	private ImageView activeAbility;
+	
+
+	public void setPlayerRef(Player selfPlayer) {
+		this.selfPlayer = selfPlayer;
+	}
+	
+	public void setActiveSet() {
+		activeRace.setImage(new Image("/images/abilitys/active/"+selfPlayer.getActiveSet().getRace().getName()+".png"));
+		activeRace.setImage(new Image("/images/abilitys/active/"+selfPlayer.getActiveSet().getAbility().getName()+".png"));
+
+	}
 
 }
