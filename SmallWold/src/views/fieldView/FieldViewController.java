addc2f1e7a71937d888eed1eac1140aa17cefaec
Marinus <Marinus@145.101.73.142>
2017-06-20 13:44:28 +0200

    Voor Wim, fieldview dingen

--- SmallWold/src/views/fieldView/FieldViewController.java
+++ SmallWold/src/views/fieldView/FieldViewController.java
@@ -1,5 +1,7 @@
 package views.fieldView;
 
+import java.beans.EventHandler;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -7,83 +9,94 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import views.sideBarView.SideBarController;
 import views.tabView.TabViewController;
 
 public class FieldViewController {
 
-    private TabViewController tabViewControl;
-    private SideBarController sideBarControl;
-    private StackPane declarePanePrevious = new StackPane();
-    private int declaredTokenAmount;
+	private TabViewController tabViewControl;
+	private SideBarController sideBarControl;
+	private StackPane declarePanePrevious = new StackPane();
+	private int declaredTokenAmount;
+	private int throughTheList;
 
-    @FXML
-    private TextField token_amount;
+	@FXML
+	private TextField token_amount;
 
-    public FieldViewController() {
+	@FXML
+	private AnchorPane mainPane;
+
+	public FieldViewController() {
 
 	}
 
-    public void setControllers(TabViewController tabControl, SideBarController sideControl ){
-    	tabViewControl = tabControl;
-    	sideBarControl = sideControl;
-    }
+	public void setControllers(TabViewController tabControl, SideBarController sideControl) {
+		tabViewControl = tabControl;
+		sideBarControl = sideControl;
+	}
+
+	@FXML
+	public void keyPressed(KeyEvent ke) {
+		if (ke.getCode().equals(KeyCode.TAB)) {
+			tabViewControl.showView();
+			sideBarControl.changeImagePressed();
+		}
+		else if(ke.getCode().equals(KeyCode.RIGHT)){
+			arrowKeyPressedRight(ke);
+		}
+		else if(ke.getCode().equals(KeyCode.LEFT)){
+			arrowKeyPressedLeft(ke);
+		}
+	}
+
+	@FXML
+	public void keyReleased(KeyEvent ke) {
+		if (ke.getCode().equals(KeyCode.TAB)) {
+			tabViewControl.hideView();
+			sideBarControl.changeImageReleased();
+		}
+	}
 
 
-    @FXML
-    private void keyPressed(KeyEvent ke) {
-    	if (ke.getCode().equals(KeyCode.TAB)){
-    		tabViewControl.showView();
-    		sideBarControl.changeImagePressed();
 
-    	}
-    }
+	@FXML
+	public void declareTokenClick(MouseEvent event) {
+		try {
+			StackPane field = (StackPane) event.getTarget();
+			StackPane declarePane = (StackPane) field.getChildren().get(0);
+			declarePane.setVisible(true);
+			this.declarePanePrevious.setVisible(false);
+			this.declarePanePrevious = declarePane;
 
-    @FXML
-    private void keyReleased(KeyEvent ke) {
-    	if (ke.getCode().equals(KeyCode.TAB)){
-    		tabViewControl.hideView();
-    		sideBarControl.changeImageReleased();
-    	}
-    }
+		} catch (ClassCastException e) {
+			System.out.println("Je hebt hier al op geklikt. ");
+		}
+	}
 
-    @FXML
-    private void declareTokenClick(MouseEvent event){
-    	try {
-    		StackPane field = (StackPane) event.getTarget();
-    		StackPane declarePane = (StackPane)field.getChildren().get(0);
-    		declarePane.setVisible(true);
-    		this.declarePanePrevious.setVisible(false);
-    		this.declarePanePrevious = declarePane;
+	@FXML
+	public void buttonMin(ActionEvent pressButtonMin) {
+		if (declaredTokenAmount > 0) {
+			declaredTokenAmount = declaredTokenAmount - 1;
+			String tokenAmount = String.valueOf(declaredTokenAmount);
+			token_amount.setText(tokenAmount);
+		}
+	}
 
-    	} 	catch (ClassCastException  e){
-    		System.out.println("Je hebt hier al op geklikt. ");
-    	}
-    }
+	@FXML
+	public void buttonPlus(ActionEvent pressButtonPlus) {
+		declaredTokenAmount = declaredTokenAmount + 1;
+		String tokenAmount = String.valueOf(declaredTokenAmount);
+		token_amount.setText(tokenAmount);
+	}
 
-    @FXML
-    private void buttonMin (ActionEvent pressButtonMin){
-    	if(declaredTokenAmount > 0){
-    		declaredTokenAmount = declaredTokenAmount - 1;
-    		String tokenAmount = String.valueOf(declaredTokenAmount);
-    		token_amount.setText(tokenAmount);
-    	}
-    }
-
-    @FXML
-    private void buttonPlus (ActionEvent pressButtonPlus){
-    	declaredTokenAmount = declaredTokenAmount + 1;
-    	String tokenAmount = String.valueOf(declaredTokenAmount);
-    	token_amount.setText(tokenAmount);
-    }
-
-    @FXML
-    private void buttonBevestig (ActionEvent pressButtonBevestig){
-    	getDeclaredTokenAmount();
-    	System.out.println(declaredTokenAmount);
-    	this.declarePanePrevious.setVisible(false);
-    }
+	@FXML
+	public void buttonBevestig(ActionEvent pressButtonBevestig) {
+		getDeclaredTokenAmount();
+		System.out.println(declaredTokenAmount);
+		this.declarePanePrevious.setVisible(false);
+	}
 
 	public int getDeclaredTokenAmount() {
 		return declaredTokenAmount;
@@ -93,5 +106,32 @@ public class FieldViewController {
 		this.declaredTokenAmount = declaredTokenAmount;
 	}
 
+	@FXML
+	public void arrowKeyPressedLeft(KeyEvent key) {
+		if (key.getCode() == KeyCode.LEFT) {
+			System.out.println("Left arrow key is pressed");
 
+			if (throughTheList > 0) {
+			throughTheList = throughTheList - 1;
+			StackPane throughTheTerrain = (StackPane) mainPane.getChildren().get(throughTheList);
+			StackPane declarePane = (StackPane) throughTheTerrain.getChildren().get(0);
+			declarePane.setVisible(true);
+			this.declarePanePrevious.setVisible(false);
+			this.declarePanePrevious = declarePane;
+			}
+		}
+	}
+	@FXML
+	public void arrowKeyPressedRight(KeyEvent key){
+		if (key.getCode() == KeyCode.RIGHT) {
+			System.out.println("Right arrow key is pressed");
+
+			throughTheList = throughTheList + 1;
+			StackPane throughTheTerrain = (StackPane) mainPane.getChildren().get(throughTheList);
+			StackPane declarePane = (StackPane) throughTheTerrain.getChildren().get(0);
+			declarePane.setVisible(true);
+			this.declarePanePrevious.setVisible(false);
+			this.declarePanePrevious = declarePane;
+		}
+	}
 }
